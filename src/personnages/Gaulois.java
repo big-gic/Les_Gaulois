a7b429dfca76235ff9a01c607a0cf6ab3d8aa893
Lacroix Ludovic <135865136+big-gic@users.noreply.github.com>
2023-09-20 15:29:49 +0200

    goulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,13 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	
+}
